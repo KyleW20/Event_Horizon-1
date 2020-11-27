94618c5ddf679315a654cab1dc4787218897697e
KyleW20 <kw18f@my.fsu.edu>
2020-11-27 18:07:13 -0500

    Selecting date of event displays date to user in TextView.

--- app/src/main/java/edu/fsu/cs/mobileproject/AddActivity.java
+++ app/src/main/java/edu/fsu/cs/mobileproject/AddActivity.java
@@ -10,28 +10,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class AddActivity extends AppCompatActivity {
+    private DatePickerDialog.OnDateSetListener dateSet;
+    private TextView dText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         //TimePickerDialog timePicker = new TimePickerDialog();
-        final int[] year = {0};
-        final int[] month = {0};
-        final int[] day = {0};
 
+        dText = (TextView) findViewById(R.id.textView3);
         Button date = (Button) findViewById(R.id.dateButton);
         date.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                final DatePickerDialog datePicker = new DatePickerDialog(AddActivity.this);
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                final DatePickerDialog datePicker = new DatePickerDialog(AddActivity.this, dateSet, year, month, day);
                 datePicker.show();
-                
+
             }
         });
+
+        dateSet = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String pass = month + "/" + day + "/" + year;
+                dText.setText(pass);
+            }
+        };
     }
 }
